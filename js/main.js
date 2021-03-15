a8f4d99e5062c2629cb1d6fb95c75f119c5b8672
Volodymyr <citynight.immortal@gmail.com>
2021-03-15 11:28:11 +0200

    close modal

--- js/main.js
+++ js/main.js
@@ -62,9 +62,9 @@ function checkIn() {
         }
     })
 }
-//
-// window.onclick = function (event) {
-//     if (event.target === modal) {
-//         modal.style.display = "none";
-//     }
-// }
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
